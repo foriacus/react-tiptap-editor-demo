f1a92e984661ebae44f965a057baa2ed00649801
foriacus <634844533@qq.com>
2024-02-24 15:15:18 +0800

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,63 @@
+import { useEffect } from "react";
+import { AiEditor } from "./core/Editor";
+
+import "./App.css";
+
+function App() {
+  useEffect(() => {
+    if (window.AiEditor) return;
+    window.AiEditor = new AiEditor({
+      element: "#aiEditor",
+      placeholder: "点击输入内容...",
+      contentRetention: true,
+      content:
+        'AiEditor 是一个面向 AI 的下一代富文本编辑器。<p> <strong>提示：</strong> <br/>1、输入 空格 + "/" 可以快速弹出 AI 菜单 <br/> 2、输入 空格 + "@" 可以提及某人</p> ',
+      i18n: {
+        zh: {
+          undo: "撤销(可自定义国际化内容...)",
+          redo: "重做(可自定义国际化内容!)",
+        },
+      },
+      onMentionQuery: (query) => {
+        return [
+          "Michael Yang",
+          "Jean Zhou",
+          "Tom Cruise",
+          "Madonna",
+          "Jerry Hall",
+          "Joan Collins",
+          "Winona Ryder",
+          "Christina Applegate",
+          "Alyssa Milano",
+          "Molly Ringwald",
+          "Ally Sheedy",
+          "Debbie Harry",
+          "Olivia Newton-John",
+          "Elton John",
+          "Michael J. Fox",
+          "Axl Rose",
+          "Emilio Estevez",
+          "Ralph Macchio",
+          "Rob Lowe",
+          "Jennifer Grey",
+          "Mickey Rourke",
+          "John Cusack",
+          "Matthew Broderick",
+          "Justine Bateman",
+          "Lisa Bonet",
+        ]
+          .filter((item) => item.toLowerCase().startsWith(query.toLowerCase()))
+          .slice(0, 5);
+      },
+    });
+  }, []);
+
+  return (
+    <>
+      <div></div>
+      <div id="aiEditor"></div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/extensions/HeadingExt.ts
@@ -0,0 +1,12 @@
+import { Heading, HeadingOptions } from "@tiptap/extension-heading";
+
+export const HeadingExt = Heading.extend<HeadingOptions>({
+  addAttributes() {
+    return {
+      ...this.parent?.(),
+      id: {
+        default: null,
+      },
+    };
+  },
+});
